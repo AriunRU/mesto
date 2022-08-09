a8ce3c440e583bcf0e1a04e64f2f20ddb6c26119
Кирилл Сидорков <ariunmlg@gmail.com>
2022-08-09 21:00:21 +0500

    MB Last

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 const popup = document.querySelector('.popup');
 const popupToggle = document.querySelector('.profile__edit-button');
-const popupClose = document.querySelector('.popup__closed');
-const popupName = document.querySelector('.popup__input_name');
-const popupProfession = document.querySelector('.popup__input_profession');
-const popupSubmit = document.querySelector('.popup__submit-btn');
+const popupClose = document.querySelector('.popup__close');
+const popupName = document.querySelector('.popup__input_type_name');
+const popupProfession = document.querySelector('.popup__input_type_profession');
 
 const popupTitle = document.querySelector('.profile__name');
 const popupSubtitle = document.querySelector('.profile__text');
